09df13774e0079e2f5a5a87a2ec604c23f659244
Joe Farrington <farrington.joe@gmail.com>
2023-01-11 10:51:08 +0000

    Bug fix to VIPolicy, add support for providing df as argument

--- viso_jax/policies/value_iteration_policy.py
+++ viso_jax/policies/value_iteration_policy.py
@@ -12,30 +12,41 @@ class VIPolicy(object):
         env_kwargs: Optional[dict] = {},
         env_params: Optional[dict] = {},
         policy_params_filepath: Optional[str] = None,
+        policy_params_df: Optional[pd.DataFrame] = None,
     ):
         if env_id not in registered_envs:
             raise ValueError("Environment ID is not registered.")
-        if policy_params_filepath:
-            env, all_env_params = viso_jax.make(env_id, **env_kwargs)
-            all_env_params = all_env_params.replace(**env_params)
+        if (policy_params_filepath is not None) and (policy_params_df is not None):
+            raise ValueError(
+                "Supply policy parameters using only one of policy_params_filepath or policy_params_df"
+            )
+        elif (policy_params_filepath is not None) or (policy_params_df is not None):
+            env, default_env_params = viso_jax.make(env_id, **env_kwargs)
+            env_params = default_env_params.create_env_params(**env_params)
             self.obs_space_shape = jnp.hstack(
                 [
-                    env.observation_space(all_env_params).high
-                    - env.observation_space(all_env_params).low
+                    env.observation_space(env_params).high
+                    - env.observation_space(env_params).low
                     + 1,
                     -1,  # Shape of action, will be squeezed out if 1
                 ]
             )
-            self.policy_params = self.load_policy_params(policy_params_filepath)
+            if policy_params_filepath:
+                policy_params_df = self._load_policy_params(policy_params_filepath)
+            self.policy_params = self._policy_params_df_to_array(policy_params_df)
 
     @classmethod
     def forward(cls, policy_params, obs, rng):
         order = policy_params[tuple(obs)]
         return order
 
-    def load_policy_params(self, filepath):
-        params_df = pd.read_csv(filepath, index_col=0)
+    def _load_policy_params_df(self, filepath):
+        policy_params_df = pd.read_csv(filepath, index_col=0)
+
+        return policy_params_df
+
+    def _policy_params_df_to_array(self, policy_params_df):
         policy_params = jnp.array(
-            params_df.values.reshape(self.obs_space_shape)
+            policy_params_df.values.reshape(self.obs_space_shape)
         ).squeeze()
         return policy_params
