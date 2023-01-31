15100190d13de068497c6554786583d7ff69320c
Joe Farrington <farrington.joe@gmail.com>
2023-01-31 11:29:31 +0000

    Add docstring and type hints to utils/ordering

--- viso_jax/utils/ordering.py
+++ viso_jax/utils/ordering.py
@@ -1,16 +1,14 @@
-# Calculate max order quantity for perishable product using newsvendor method
-# Assumes demand is poisson distributed
-
 import math
 from scipy.stats import poisson
 
 
 def calculate_newsvendor_order_quantity_poisson_demand(
-    demand_poisson_mean, max_useful_life, sales_price, variable_order_cost
-):
+    demand_poisson_mean: float,
+    max_useful_life: float,
+    sales_price: float,
+    variable_order_cost: float,
+) -> int:
+    """Calculate newsvendor order quantity for a  perishable product with poisson demand based on
+    Equation 2 of Hendrix et al (2019)"""
     quantile = (sales_price - variable_order_cost) / sales_price
     return math.ceil(poisson.ppf(quantile, max_useful_life * demand_poisson_mean))
-
-
-def hand_coded_order_quantity(order_quantity):
-    return order_quantity
