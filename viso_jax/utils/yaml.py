f18b48ba5f4b5616ac35dc78332c5f57c2611377
Joe Farrington <farrington.joe@gmail.com>
2023-01-31 10:59:05 +0000

    Add docstring and type hints to utils/yaml

--- viso_jax/utils/yaml.py
+++ viso_jax/utils/yaml.py
@@ -1,14 +1,14 @@
 import yaml
 
 
-def to_yaml(data, filepath):
-    """A function to write to YAML file"""
+def to_yaml(data: dict, filepath: str) -> None:
+    """Write a dict to YAML file"""
     with open(filepath, "w") as f:
         yaml.safe_dump(data, f, sort_keys=False)
 
 
-def from_yaml(filepath):
-    """A function to load from a YAML file"""
+def from_yaml(filepath: str) -> dict:
+    """Load from a YAML file"""
     with open(filepath) as f:
         data = yaml.safe_load(f)
     return data
