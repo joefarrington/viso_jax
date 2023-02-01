38c328817b453255074cc3d587e7edd48d12dc60
Joe Farrington <farrington.joe@gmail.com>
2023-02-01 17:08:13 +0000

    Update get kpi function to use make

--- viso_jax/utils/kpis.py
+++ viso_jax/utils/kpis.py
@@ -1,23 +1,8 @@
-from viso_jax.environments import (
-    DeMoorPerishableGymnax,
-    HendrixPerishableOneProductGymnax,
-    HendrixPerishableSubstitutionTwoProductGymnax,
-    MirjaliliPerishablePlateletGymnax,
-)
+from viso_jax.registration import make
 from typing import Callable
 
 
 def get_kpi_function(env_id: str, **env_kwargs) -> Callable:
     """Return the KPI function for the given environment ID."""
-    if env_id == "DeMoorPerishable":
-        env = DeMoorPerishableGymnax(env_kwargs)
-    elif env_id == "HendrixPerishableOneProduct":
-        env = HendrixPerishableOneProductGymnax(**env_kwargs)
-    elif env_id == "HendrixPerishableSubstitutionTwoProduct":
-        env = HendrixPerishableSubstitutionTwoProductGymnax(**env_kwargs)
-    elif env_id == "MirjaliliPerishablePlatelet":
-        env = MirjaliliPerishablePlateletGymnax(**env_kwargs)
-
-    else:
-        raise ValueError("Environment ID is not registered.")
+    env, _ = make(env_id, **env_kwargs)
     return env.calculate_kpis
