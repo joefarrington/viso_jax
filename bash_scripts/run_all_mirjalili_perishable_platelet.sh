a740900772c3ec005e91ef91fd71410146ddf55d
Joe Farrington <farrington.joe@gmail.com>
2023-02-05 15:44:45 +0000

    Add missing closing quotation mark at end

--- bash_scripts/run_all_mirjalili_perishable_platelet.sh
+++ bash_scripts/run_all_mirjalili_perishable_platelet.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-scenario=de_moor_perishable
-max_useful_lives=( 2 3 4 5)
-experiments=( 1 2 3 4 5 6 7 8)
+scenario=mirjalili_perishable_platelet
+max_useful_lives=( 3 5 8)
+experiments=( 1 2 )
 date_now=$(date +"%Y-%m-%d")
 time_now=$(date +"%H-%M-%S")
 
@@ -11,7 +11,12 @@ echo "Running all experiments for scenario ${scenario} at ${date_now} ${time_now
 cd ../viso_jax/value_iteration
 
 for m in ${max_useful_lives[@]}
-do
+do  
+    if [ $m -gt 3 ]
+    then
+        echo "Skipping m=${m}" # m=5 takes a long time, m=8 we can't do so skip for today
+        continue
+    fi
     for exp in ${experiments[@]}
     do  
         echo "Running value iteration for m=${m}, exp=${exp}"
